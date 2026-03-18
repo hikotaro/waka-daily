60a6caa3be767506acdbc6f9d90be0e4108b5d31
hikotaro <hikotaro_san@yahoo.co.jp>
2026-03-19 08:10:41 +0900

    feat: add responsive design to Waka component for mobile and tablet

--- src/components/Waka.jsx
+++ src/components/Waka.jsx
@@ -4,15 +4,11 @@ export const Waka = ({ waka }) => {
   return (
     <div className='waka-wrapper'>
       <div className='waka-card'>
-        {waka.number && <span className='waka-number'>No. {waka.number}</span>}
+        {waka.number && <span className='waka-number'>第{waka.number}番</span>}
         <p className='waka-original'>{waka.text.original}</p>
         <p className='waka-reading'>{waka.text.reading}</p>
-        <div className='waka-divider' />
-        <div className='waka-author'>
-          <span className='waka-author-name'>{waka.author.name}</span>
-          <span className='waka-author-reading'>（{waka.author.reading}）</span>
-          <span className='waka-author-era'>{waka.author.era}</span>
-        </div>
+        <p className='waka-author'>{waka.author}</p>
+        {waka.season && <p className='waka-season'>{waka.season}</p>}
       </div>
     </div>
   );
